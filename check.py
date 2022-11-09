5cba555ea9af94cf05316cdddea16752b607ee5b
Saurav Thakur <sauravthakur1341@gmail.com>
2022-11-09 23:03:15 +0545

    Delete check.py

--- check.py
+++
@@ -1,6 +0,0 @@
-import os
-from sensor.constant.env_variable import MONGODB_URL_KEY
-# from pandas import DataFrame
-# mongo = os.getenv("MONGODB_URL_KEY")
-# print(mongo)
-print(os.getenv(MONGODB_URL_KEY))
